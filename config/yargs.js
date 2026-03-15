e9612732c7577b0e9913d9cfcf0a999c9407b078
Julian Ortiz <patula84@hotmail.com>
2026-03-15 17:09:30 -0500

    primer commit

---
+++ config/yargs.js
@@ -0,0 +1,35 @@
+
+const yargs = require('yargs/yargs');
+
+const argv = yargs(process.argv.slice(2))
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe : 'Es la base de la tabla de multiplicar'
+    })
+        .option('h', {
+        alias: 'hasta',
+        type: 'number',
+        default: 10,
+        describe : 'Este es el numero hasta donde quieres la tabla'
+    })
+        .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false,
+        describe: 'Muestra la tabla en consola'
+        
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base debe ser un numero'
+            
+        }
+        return true;
+        
+    })
+    .argv;
+
+
+    module.exports = argv;
